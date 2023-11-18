6be213e76c36a1dcf07015f87d6cd69c322ba1ef
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-18 11:36:15 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -10,23 +10,23 @@
     <div class = "container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">About</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Courses</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="doctors.php">Doctors</a>
+          <a class="nav-link" href="doctors.php">Admissions</a>
         </li>
              <li class="nav-item">
-          <a class="nav-link" href="patients.php">Patients</a>
+          <a class="nav-link" href="patients.php">Academics</a>
         </li>
              <li class="nav-item">
-          <a class="nav-link" href="doctors-with-patients.php">Doctors with Patients</a>
+          <a class="nav-link" href="doctors-with-patients.php">School Activities</a>
         </li>
       </ul>
     </div>
