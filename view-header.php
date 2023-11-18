16d85722fb58416cc7f20f816be5a26402b30586
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-18 12:07:21 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,17 +7,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
         .navbar {
-            padding: 100px; /* Adjust the padding as needed */
+            /* Remove padding and use mx-auto to center the navbar */
+            padding: 0;
+            margin-left: auto;
+            margin-right: auto;
         }
 
         .navbar-brand {
             font-size: 60px; /* Adjust the font size of the brand as needed */
         }
+
         /* Add custom styles for navbar elements spacing */
         .navbar-nav .nav-item {
-            margin-right: 100px; /* Adjust the margin as needed */
-            margin-left: 100px
-            
+            margin-right: 50px; /* Adjust the margin as needed */
         }
     </style>
 </head>
@@ -32,7 +34,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                         <li class="nav-item">
+                        <li class="nav-item">
                             <a class="nav-link" href="doctors.php">About</a>
                         </li>
                         <li class="nav-item">
