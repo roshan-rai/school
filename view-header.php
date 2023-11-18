187abde0f43f9729acd9ba387fb7ba9686b16931
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-18 12:04:33 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,6 +6,13 @@
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <style>
+        .navbar {
+            padding: 100px; /* Adjust the padding as needed */
+        }
+
+        .navbar-brand {
+            font-size: 60px; /* Adjust the font size of the brand as needed */
+        }
         /* Add custom styles for navbar elements spacing */
         .navbar-nav .nav-item {
             margin-right: 100px; /* Adjust the margin as needed */
