3a64f6b94f4251d8b9eab3a0b39f39109d6387e9
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-18 11:43:37 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,10 +7,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
   <body>
-  <div class="container" style="margin-left:auto; margin-right:auto; padding-left:15px; padding-right:15px; width:100%; border-top: 10px solid skyblue;">
+  <div class="container">
       
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+  <div class="container-fluid"  style="margin-left:auto; margin-right:auto; padding-left:15px; padding-right:15px; width:100%; border-top: 10px solid skyblue;">
     <a class="navbar-brand" href="#">About</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
