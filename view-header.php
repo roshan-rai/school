c193be2b01286de4cbda349059c1059159dae390
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-18 11:42:51 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,7 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
   <body>
-    <div class = "container">
+  <div class="container" style="margin-left:auto; margin-right:auto; padding-left:15px; padding-right:15px; width:100%; border-top: 10px solid skyblue;">
+      
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">About</a>
