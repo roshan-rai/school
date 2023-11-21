bef3cc2d7c28b5ba5a5385494df632ca33968649
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-21 11:56:52 -0600

    Update about.php

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
       background-image: url('giphy.gif');
       background-size: cover;
       height: 100vh;
-      width: 100%; /
+      width: 100%; 
       position: relative;
       background-color: rgba(255, 255, 255, 0.8); 
     }
