d7aed8e7225a1385b6f513e963fa20ef206b2c2f
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-21 11:18:32 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,15 +1,12 @@
-            background-image: url('video.png'); /* Set your header container background image */
-
-<p style="font-size: 50px;">
-          <ul>
-            <li style="font-size: 50px;">About Us</li>
-              <li>Hogwarts School of Witchcraft and Wizardry, nestled amidst the enchanting landscape of the British countryside, is a place where magic comes to life and young witches and wizards embark on an extraordinary journey of discovery.</li>
-              <li>Location: British Old Castle</li>
-              <li>Headmaster: Albus Dumbledore</li>
-              <li>Means of Transportation: Hogwarts Express, Platform 9¾</li>
-              <li>Values: Friendship, courage, and the pursuit of truth</li>
-            </ul>
-
-            
-        </p>
-
+<div style="background-image: url('video.png');">
+  <p style="font-size: 50px;">
+    <ul style="list-style-type: none; padding: 0;">
+      <li style="font-size: 50px;">About Us</li>
+      <li>Hogwarts School of Witchcraft and Wizardry, nestled amidst the enchanting landscape of the British countryside, is a place where magic comes to life and young witches and wizards embark on an extraordinary journey of discovery.</li>
+      <li>Location: British Old Castle</li>
+      <li>Headmaster: Albus Dumbledore</li>
+      <li>Means of Transportation: Hogwarts Express, Platform 9¾</li>
+      <li>Values: Friendship, courage, and the pursuit of truth</li>
+    </ul>
+  </p>
+</div>
