5c66cf9a738be56a3cf0a79888df72e7db4fe2a6
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-21 12:29:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 
 ?>
-    <h1>School </h1>
+    <h1>Help will always be given at Hogwarts to those who ask for it - Albus Dumbledore </h1>
   <?php
 include "view-footer.php";
 ?>
