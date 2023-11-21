31f5118ec8004c04b83978d9b9462b7a672d74d6
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-21 11:45:59 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,12 +1,38 @@
-<div style="background-image: url('giphy.gif');">
-  <p style="font-size: 50px;">
-    <ul style="list-style-type: none; padding: 0;">
-      <li style="font-size: 50px;">About Us</li>
-      <li>Hogwarts School of Witchcraft and Wizardry, nestled amidst the enchanting landscape of the British countryside, is a place where magic comes to life and young witches and wizards embark on an extraordinary journey of discovery.</li>
-      <li>Location: British Old Castle</li>
-      <li>Headmaster: Albus Dumbledore</li>
-      <li>Means of Transportation: Hogwarts Express, Platform 9¾</li>
-      <li>Values: Friendship, courage, and the pursuit of truth</li>
-    </ul>
-  </p>
-</div>
+ .background-container {
+      background-image: url('giphy.gif');
+      background-size: cover;
+      height: 100vh;
+      width: 50%;
+    }
+
+    .content {
+      width: 50%;
+      padding: 20px;
+    }
+
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+
+    li {
+      font-size: 20px; /* Adjust the font size as needed */
+    }
+  </style>
+</head>
+<body>
+
+  <div class="background-container"></div>
+
+  <div class="content">
+    <p style="font-size: 50px;">
+      <ul>
+        <li style="font-size: 50px;">About Us</li>
+        <li>Hogwarts School of Witchcraft and Wizardry, nestled amidst the enchanting landscape of the British countryside, is a place where magic comes to life and young witches and wizards embark on an extraordinary journey of discovery.</li>
+        <li>Location: British Old Castle</li>
+        <li>Headmaster: Albus Dumbledore</li>
+        <li>Means of Transportation: Hogwarts Express, Platform 9¾</li>
+        <li>Values: Friendship, courage, and the pursuit of truth</li>
+      </ul>
+    </p>
+  </div>
