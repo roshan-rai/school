acdb30fbb6931f93f988ec352fb9132c1ae5b722
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-20 19:41:19 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,48 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your School Name</title>
     <style>
-        /* Remove padding and use mx-auto to center the navbar */
-        .navbar {
-            padding: 100;
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4; /* Set your background color */
         }
 
-        .navbar-brand {
-            font-size: 60px; /* Adjust the font size of the brand as needed */
+        /* Navigation Bar Styles */
+        nav {
+            background-color: #333; /* Set your navigation bar color */
+            color: white;
+            padding: 10px;
+            text-align: center;
         }
 
-        /* Add custom styles for navbar elements spacing */
-        .navbar-nav .nav-item {
-            margin-right: 50px;
-            margin-left: 50px;/* Adjust the margin as needed */
+        nav a {
+            color: white;
+            text-decoration: none;
+            margin: 0 15px;
+        }
+
+        /* Logo and School Name Styles */
+        .header-container {
+            background-color: #333; /* Set your header container color */
+            text-align: center;
+            padding: 20px;
+            color: white;
+        }
+
+        .school-logo {
+            max-width: 100px; /* Set your logo size */
+            max-height: 100px; /* Set your logo size */
+            display: block;
+            margin: 0 auto;
         }
     </style>
 </head>
-<body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="about.php">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admissions.php">Admissions</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="academics.php">Academics</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="activites.php">Activities</a>
-                </li>
-                 <li class="nav-item">
-                    <a class="nav-link" href="class.php">Class</a>
-                </li>
-                <li class="nav
+<body>
+    <!-- Navigation Bar -->
+    <nav>
+        <a href="#">About</a>
+        <a href="#">Admissions</a>
+        <a href="#">Academics</a>
+        <a href="#">Activities</a>
+        <a href="#">Class</a>
+        <!-- Add more navigation links as needed -->
+    </nav>
+
+    <!-- Header Container with Logo and School Name -->
+    <div class="header-container">
+        <!-- Replace 'logo.png' with your actual logo file -->
+        <img src="logo.png" alt="School Logo" class="school-logo">
+        <h1>Dream School</h1>
+    </div>
+</body>
+
+</html>
