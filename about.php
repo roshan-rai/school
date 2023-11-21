1c62c5aad0d4729bf623260c0b4395215d598fff
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-20 22:55:06 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,13 +1,13 @@
 
-<p style="font-size: 20px;">
-          <ol>
-                      <li>About Us</li>
+<p style="font-size: 50px;">
+          <ul>
+            <li style="font-size: 50px;">About Us</li>
               <li>Hogwarts School of Witchcraft and Wizardry, nestled amidst the enchanting landscape of the British countryside, is a place where magic comes to life and young witches and wizards embark on an extraordinary journey of discovery.</li>
               <li>Location: British Old Castle</li>
               <li>Headmaster: Albus Dumbledore</li>
               <li>Means of Transportation: Hogwarts Express, Platform 9¾</li>
               <li>Values: Friendship, courage, and the pursuit of truth</li>
-            </ol>
+            </ul>
 
             
         </p>
