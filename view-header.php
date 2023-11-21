86b16fbbbcb90ae57996ff9e22c8ccda1afe4a19
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-20 20:46:51 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -81,6 +81,14 @@
         <a href="#academics">Academics</a>
         <a href="#activities">Activities</a>
         <a href="#class">Class</a>
+        class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+        <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+
         <!-- Add more navigation links as needed -->
     </nav>
 
@@ -102,14 +110,7 @@
         <p>Your admissions content goes here.</p>
     </section>
 
-    <nav class="navbar bg-body-tertiary">
-      <div class="container-fluid">
-        <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+
 
 </body>
 
