1c1e2bb474125ff7291fa075ef3e62f1dac48300
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-20 20:07:40 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your School Name</title>
+    <title>Dream School</title>
     <style>
         body {
             margin: 0;
@@ -15,7 +15,7 @@
 
         /* Navigation Bar Styles */
         nav {
-            background-color: #333; /* Set your navigation bar color */
+            background-color: skyblue; /* Set your navigation bar color */
             color: white;
             padding: 10px;
             text-align: center;
