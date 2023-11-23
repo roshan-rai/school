3cfd53ad40589fcf45f94cd129e8ed89d6814c9b
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-22 23:10:09 -0600

    Update admissions.php

--- admissions.php
+++ admissions.php
@@ -1,20 +1,24 @@
-<div class="row">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-</div>
+ <div class="row">
+        <!-- Card 1 -->
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="AllFairy.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h2 class="card-title">Fairies</h2>
+                    <p class="card-text">More fairies.</p>
+                    <a href="listFairies.html" class="btn btn-primary">Let's go into the Fairy World !!!</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Card 2 -->
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="pirates.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h2 class="card-title">Pirates</h2>
+                    <p class="card-text">More Pirates.</p>
+                    <a href="listPirates.html" class="btn btn-primary">Let's go into the Pirates World !!!</a>
+                </div>
+            </div>
+        </div>
