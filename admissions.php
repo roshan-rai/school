c03e4cad80d62e16b154bfa164094712d817b13d
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-22 23:07:14 -0600

    Update admissions.php

--- admissions.php
+++ admissions.php
@@ -1,5 +1,5 @@
 <div class="row">
-  <div class="col-sm-6 mb-3 mb-sm-0">
+  <div class="col-sm-6">
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
