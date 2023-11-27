040c731ccaa7a35a8b2784b8483de15db8efc768
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 13:40:05 -0600

    Update admissions.php

--- admissions.php
+++ admissions.php
@@ -1,36 +1,46 @@
-<head>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!DOCTYPE html>
+<html lang="en">
 
- <section class="main-content">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <section class="main-content">
         <table class="table">
             <tbody>
                 <tr>
                     <td>
-                     <h1>Apply to be a part of Hogwarts</h1>
+                        <h1>Apply to be a part of Hogwarts</h1>
                         <!-- my professional -->
                         <p>
-                            Welcome to Hogwarts School of Witchcraft and Wizardry Admissions
+                            <strong>Welcome to Hogwarts School of Witchcraft and Wizardry Admissions</strong><br>
+                            <em>Enchanting Futures, One Student at a Time</em><br><br>
 
-Enchanting Futures, One Student at a Time
+                            <strong>Dear Prospective Students and Families,</strong><br><br>
 
-Dear Prospective Students and Families,
+                            <strong>Welcome to the magical world of Hogwarts!</strong> We are delighted that you are considering joining our esteemed institution, where centuries of tradition meet modern wizarding education.<br><br>
 
-Welcome to the magical world of Hogwarts! We are delighted that you are considering joining our esteemed institution, where centuries of tradition meet modern wizarding education.
-
-Admission Process:
-
-Eligibility: Hogwarts welcomes young witches and wizards between the ages of 11 and 17. We encourage all magical folks to apply, regardless of their magical background or family lineage.
-
-Application Submission: To embark on your magical journey, submit your application through our enchanted online portal. Be prepared to share your magical achievements, experiences, and any notable magical talents.
-
-Letters of Acceptance: Successful applicants will receive their acceptance letters via owl post before the start of the new school year. Keep an eye on the skies for your personal owl messenger!
+                            <strong>Admission Process:</strong><br>
+                            <ul>
+                                <li><strong>Eligibility:</strong> Hogwarts welcomes young witches and wizards between the ages of 11 and 17. We encourage all magical folks to apply, regardless of their magical background or family lineage.</li>
+                                <li><strong>Application Submission:</strong> To embark on your magical journey, submit your application through our enchanted online portal. Be prepared to share your magical achievements, experiences, and any notable magical talents.</li>
+                                <li><strong>Letters of Acceptance:</strong> Successful applicants will receive their acceptance letters via owl post before the start of the new school year. Keep an eye on the skies for your personal owl messenger!</li>
+                            </ul>
                         </p>
+                        <!-- Apply Button -->
+                        <a href="apply.php" class="btn btn-primary">Apply Now</a>
                     </td>
                     <td>
                         <!-- my professional -->
-                        <img src=" " alt="Picture">
+                        <img src="aboutimage.jpg" alt="Picture">
                     </td>
                 </tr>
             </tbody>
         </table>
     </section>
+
+</body>
+
+</html>
