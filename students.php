7de1d788d80846dd1a64c94b8973bb64718996fa
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 14:14:04 -0600

    Update and rename student to students.php

---
+++ students.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-students.php");
+
+$pageTitle = "Students";
+include "view-header.php";
+$students = selectStudents();
+include "view-students.php";
+include "view-footer.php";
+?>
