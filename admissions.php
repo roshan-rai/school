45ef8af146e145fde75fbf30e09418337b0bef2c
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 13:58:07 -0600

    Update admissions.php

--- admissions.php
+++ admissions.php
@@ -5,8 +5,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .table img {
-            max-height: 1200px;
-            max-width: 500px;
+               max-height: 621px;
+                max-width: 1508px;
         }
     </style>
 </head>
