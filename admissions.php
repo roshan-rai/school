22ac0037d53c672b93cf7e0a9b078c23d0ca2fc1
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 13:35:50 -0600

    Update admissions.php

--- admissions.php
+++ admissions.php
@@ -1,28 +1,36 @@
 <head>
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-</head>
-<div class="row">
-        <!-- Card 1 -->
-        <div class="col-md-4 mb-4">
-            <div class="card">
-                <img src="AllFairy.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h2 class="card-title">Fairies</h2>
-                    <p class="card-text">More fairies.</p>
-                    <a href="listFairies.html" class="btn btn-primary">Let's go into the Fairy World !!!</a>
-                </div>
-            </div>
-        </div>
+ <section class="main-content">
+        <table class="table">
+            <tbody>
+                <tr>
+                    <td>
+                     <h1>Apply to be a part of Hogwarts</h1>
+                        <!-- my professional -->
+                        <p>
+                            Welcome to Hogwarts School of Witchcraft and Wizardry Admissions
 
-        <!-- Card 2 -->
-        <div class="col-md-4 mb-4">
-            <div class="card">
-                <img src="pirates.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h2 class="card-title">Pirates</h2>
-                    <p class="card-text">More Pirates.</p>
-                    <a href="listPirates.html" class="btn btn-primary">Let's go into the Pirates World !!!</a>
-                </div>
-            </div>
-        </div>
+Enchanting Futures, One Student at a Time
+
+Dear Prospective Students and Families,
+
+Welcome to the magical world of Hogwarts! We are delighted that you are considering joining our esteemed institution, where centuries of tradition meet modern wizarding education.
+
+Admission Process:
+
+Eligibility: Hogwarts welcomes young witches and wizards between the ages of 11 and 17. We encourage all magical folks to apply, regardless of their magical background or family lineage.
+
+Application Submission: To embark on your magical journey, submit your application through our enchanted online portal. Be prepared to share your magical achievements, experiences, and any notable magical talents.
+
+Letters of Acceptance: Successful applicants will receive their acceptance letters via owl post before the start of the new school year. Keep an eye on the skies for your personal owl messenger!
+                        </p>
+                    </td>
+                    <td>
+                        <!-- my professional -->
+                        <img src=" " alt="Picture">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
