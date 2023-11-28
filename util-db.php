f77fbb0a22bb4c7d75f3d29ad95211b67e5a0d3e
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 20:21:47 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44','roshanou_schooluser', 'MIS4013-001', 'roshanou_schooluser');
+    $conn = new mysqli('159.89.47.44','roshanou_schooluser', 'MIS4013-001', 'roshanou_school');
     
     // Check connection
     if ($conn->connect_error) {
