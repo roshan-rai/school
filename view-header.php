d9997032c3cf51e616d4dd5c3c4d5802d497df96
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 20:14:41 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -122,7 +122,13 @@
             include "admissions.php";
         ?>
     </section>
-
+    
+<section id="student" style="background-color: #eee;">
+        <h2>Student</h2>
+        <?php
+            include "students.php";
+        ?>
+    </section>
 
 
       <section id="House" style="background-color: #eee;">
