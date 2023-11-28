4fad0460cfd7b1b5a53c2cb45af0a5d1b8aa1223
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 20:42:42 -0600

    Create view-awards.php

---
+++ view-awards.php
@@ -0,0 +1,35 @@
+<h1>Awards</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+         <th>Award Id</th>
+      <th>Name</th>
+      <th>House</th>
+         <th></th>
+      </tr>
+     
+    </thead>
+      <tbody>
+        <?php
+            while ($award = $awards-> fetch_assoc())
+              {
+                ?>
+                  <tr>
+                    <td><?php echo $award['award_id'];?></td>
+                    <td>
+                      <?php echo $award['award_name'];?></td>
+                    </td>
+                    <td>
+                      <?php echo $award['house_id'];?></td>
+                    </td>
+                    <td><a href="awards-by-house.php?id=<?php echo $award['award_id'];?>">Awards</a></td>
+                  </tr>
+                  
+                <?php
+              }
+        ?>
+      </tbody>
+    
+  </table>
+</div>
