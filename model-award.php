d764f1560f673ac4235c228c132e892a9689af80
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 20:39:36 -0600

    Update model-award.php

--- model-award.php
+++ model-award.php
@@ -1,1 +1,16 @@
 
+<?php
+function selectAwards() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT award_id,award_name,house_id FROM `award` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
