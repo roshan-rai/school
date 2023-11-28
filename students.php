398c7edb2710df12081a22a11a7281a516a9b025
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 20:15:22 -0600

    Update students.php

--- students.php
+++ students.php
@@ -2,9 +2,6 @@
 require_once("util-db.php");
 require_once("model-students.php");
 
-$pageTitle = "Students";
-include "view-header.php";
 $students = selectStudents();
 include "view-students.php";
-include "view-footer.php";
 ?>
