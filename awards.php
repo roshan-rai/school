b4dfb516080b02d4bd982cd58dab36b7396f426c
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-11-27 20:44:53 -0600

    Update awards.php

--- awards.php
+++ awards.php
@@ -3,8 +3,7 @@ require_once("util-db.php");
 require_once("model-awards.php");
 
 $pageTitle = "Awards";
-include "view-header.php";
+
 $awards = selectAwards();
 include "view-awards.php";
-include "view-footer.php";
 ?>
