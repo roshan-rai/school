9593921b383c522ff6890a4009b18a965045703f
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 12:11:34 -0600

    Update view-student-editform.php

--- view-student-editform.php
+++ view-student-editform.php
@@ -13,7 +13,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="editCourseModalLabel<?php echo $student['student_id']; ?>">Edit Students</h1>
-          <button type="button" class="btn-close" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
           <form method="post" action="">
