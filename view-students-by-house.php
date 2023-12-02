4302dadb10d243c077cb24d765b7b855a8b7a2fd
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 16:25:48 -0600

    Create view-students-by-house.php

---
+++ view-students-by-house.php
@@ -0,0 +1,27 @@
+<h1>Student for House</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+         <th>Student ID</th>
+      <th>Student Name</th>
+      <th>Class</th>
+      </tr>
+     
+    </thead>
+      <tbody>
+        <?php
+            while ($student = $students-> fetch_assoc())
+              {
+                ?>
+                  <tr>
+                    <td><?php echo $student['student_id'];?></td>
+                    <td><?php echo $student['student_name'];?></td>
+                    <td> <?php echo $student['student_Class'];?></td>
+                  </tr>                  
+                <?php
+              }
+        ?>
+      </tbody>    
+  </table>
+</div>
