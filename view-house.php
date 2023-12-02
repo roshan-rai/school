5e5e0ef30be89cb07a71d61a5ae25c99298867ac
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 17:50:11 -0600

    Update view-house.php

--- view-house.php
+++ view-house.php
@@ -35,15 +35,32 @@
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
-                Students button clicked!
-                Students button clicked!
-                Students button clicked!
-                Students button clicked!
-                Students button clicked!
-                Students button clicked!
-                Students button clicked!
-                Students button clicked!
-                Students button clicked!
+                 <div class="table-responsive">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                         <th>Student ID</th>
+                      <th>Student Name</th>
+                      <th>Class</th>
+                      </tr>
+                     
+                    </thead>
+                      <tbody>
+                        <?php
+                            while ($student = $students-> fetch_assoc())
+                              {
+                                ?>
+                                  <tr>
+                                    <td><?php echo $student['student_id'];?></td>
+                                    <td><?php echo $student['student_name'];?></td>
+                                    <td> <?php echo $student['student_class'];?></td>
+                                  </tr>                  
+                                <?php
+                              }
+                        ?>
+                      </tbody>    
+                  </table>
+            </div>
             </div>
         </div>
     </div>
