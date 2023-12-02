7550992ca0f5437489fa1f4f205e88a6e9b7111f
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 12:51:46 -0600

    Update model-awards.php

--- model-awards.php
+++ model-awards.php
@@ -3,7 +3,7 @@
 function selectAwards() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT award_id,award_name,house_id FROM `award` ");
+        $stmt = $conn->prepare("SELECT award_id,award_name,h.house_id,house_name FROM `award` a join house h on h.house_id=a.house_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
