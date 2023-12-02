60376e14c4b2448f8c271ab343eee1d0546fac2e
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 17:12:14 -0600

    Update view-house.php

--- view-house.php
+++ view-house.php
@@ -7,33 +7,29 @@
  
 
     <div class="card" style="width: 18rem;">
-  <img src="aboutimage.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-                 <h5 class="card-title"><?php echo $house['house_name']; ?></h5>
-                <p class="card-text">Captain: <?php echo $house['house_captain']; ?></p>
-                <p class="card-text">Color: <?php echo $house['house_color']; ?></p>
-                   <div>
-                        <form method="post" action="students-by-house.php">
-                        <input type="hidden" name="hid" value="<?php echo $house['house_id'];?>"/>
-                        <button id="studentByHousebtn" type="submit" class="btn btn-primary">Students</button>
-                        </form>
+        <img src="aboutimage.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $house['house_name']; ?></h5>
+            <p class="card-text">Captain: <?php echo $house['house_captain']; ?></p>
+            <p class="card-text">Color: <?php echo $house['house_color']; ?></p>
+            <div class="accordion accordion-flush" id="accordionFlushExample<?php echo $house['house_id']; ?>">
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne<?php echo $house['house_id']; ?>" aria-expanded="false" aria-controls="flush-collapseOne<?php echo $house['house_id']; ?>">
+                            Students
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne<?php echo $house['house_id']; ?>" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample<?php echo $house['house_id']; ?>">
+                        <div class="accordion-body">
+                            <form method="post" action="students-by-house.php">
+                                <input type="hidden" name="hid" value="<?php echo $house['house_id']; ?>"/>
+                            </form>
+                        </div>
                     </div>
-      <div class="accordion accordion-flush" id="accordionFlushExample">
-      <div class="accordion-item">
-        <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-           Students
-          </button>
-        </h2>
-        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-          <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                </div>
+            </div>
         </div>
-      </div>
-</div>
-
-      
-  </div>
-</div>
+    </div>
     <?php
     }
     ?>
