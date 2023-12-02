7a87a2cb83678a45b4216b6e713548c23df49745
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 17:30:41 -0600

    Update view-students-by-house.php

--- view-students-by-house.php
+++ view-students-by-house.php
@@ -1,5 +1,13 @@
-<h1>Student for House</h1>
-<div class="table-responsive">
+<div class="toast-container position-fixed bottom-0 end-0 p-3">
+  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="..." class="rounded me-2" alt="...">
+      <strong class="me-auto">Bootstrap</strong>
+      <small>11 mins ago</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+     <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
@@ -25,3 +33,9 @@
       </tbody>    
   </table>
 </div>
+    </div>
+  </div>
+</div>
+
+<h1>Student for House</h1>
+
