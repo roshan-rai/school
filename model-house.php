cef2cdb81f65b8f380b1ba059196953132754884
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 11:07:07 -0600

    Update model-house.php

--- model-house.php
+++ model-house.php
@@ -1,23 +1,8 @@
 <?php
-function selectStudents() {
+function selectHouses() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT doctor_id, doctor_name, doctor_area FROM `doctor` ");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-function selectPatientsByDoctor($did) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT treatment_id,d.doctor_name, t.doctor_id, p.patient_id, patient_name, patient_description, treatment_name, treatment_startdate, treatment_enddate FROM patient p JOIN treatment t ON t.patient_id = p.patient_id JOIN doctor d on d.doctor_id=t.doctor_id WHERE t.doctor_id = ?;");
-        $stmt->bind_param("i", $did);
+        $stmt = $conn->prepare("SELECT house_id, house_name, house_captain, house_color FROM `house` ");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -29,35 +14,6 @@ function selectPatientsByDoctor($did) {
 }
 
 
-function selectStudentsForInput() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT student_id, student_name FROM `student` order by student_name ");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-
-
-function selectAwardForInput() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT award_id, award_name FROM `award` order by award_name ");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
 
 
 function deleteHouseWithAwards($tid) {
@@ -89,19 +45,6 @@ function updateAwardWithHouse($tid, $did, $pid, $treatment_name, $sDate, $eDate)
     }
 }
 
-function insertDoctorWithPatients( $did, $pid, $treatment_name, $sDate, $eDate) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `treatment` ( `doctor_id`, `patient_id`, `treatment_name`, `treatment_startdate`, `treatment_enddate`) VALUES (?, ?, ?, ?, ?);");
-        $stmt->bind_param("iisss",  $did, $pid,$treatment_name, $sDate, $eDate);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        console.log("error",$e);
-        $conn->close();
-        throw $e;
-    }
-}
+
 
 ?>
