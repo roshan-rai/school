4575008b63cb08b94aeb4b8d4c14ab4dc5f26d0d
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 13:03:11 -0600

    Update view-house.php

--- view-house.php
+++ view-house.php
@@ -2,17 +2,30 @@
 
 <div class="card-deck">
     <?php
+    $count = 0;
     while ($house = $houses->fetch_assoc()) {
+        if ($count % 2 == 0) {
+            echo '<div class="row">';
+        }
     ?>
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $house['house_name']; ?></h5>
-                <p class="card-text">Captain: <?php echo $house['house_captain']; ?></p>
-                <p class="card-text">Color: <?php echo $house['house_color']; ?></p>
-                <a href="house.php?id=<?php echo $house['house_id']; ?>" class="btn btn-primary">Students</a>
+
+        <div class="col-md-6">
+            <div class="card" style="width: 18rem;">
+                <img src="aboutimage.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $house['house_name']; ?></h5>
+                    <p class="card-text">Captain: <?php echo $house['house_captain']; ?></p>
+                    <p class="card-text">Color: <?php echo $house['house_color']; ?></p>
+                    <a href="house.php?id=<?php echo $house['house_id']; ?>" class="btn btn-primary">Students</a>
+                </div>
             </div>
         </div>
+
     <?php
+        if ($count % 2 == 1 || $count == $total_houses - 1) {
+            echo '</div>';
+        }
+        $count++;
     }
     ?>
 </div>
