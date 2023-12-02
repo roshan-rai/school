cddaedfa0d817d2fe0314b24fe132179f756c550
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 12:49:30 -0600

    Update house.php

--- house.php
+++ house.php
@@ -3,38 +3,7 @@ require_once("util-db.php");
 require_once("model-house.php");
 
 $pageTitle = "House";
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-        if(insertDoctorWithAwards( $_POST['did'], $_POST['pid'], $_POST['treatment_name'], $_POST['sDate'], $_POST['eDate']) )
-      {
-        echo '<div class="alert alert-success" role="alert"> Information successfully added!</div>';
-      }
-      else
-      {
-       echo '<div class="alert alert-danger" role="alert"> Error in Adding!</div>';
-      }
-      break;
-    case "Edit":
-        if( updateStudentWithAwards($_POST['tid'], $_POST['did'], $_POST['pid'], $_POST['treatment_name'], $_POST['sDate'], $_POST['eDate']))
-      {
-        echo '<div class="alert alert-success" role="alert"> Information successfully edited!</div>';
-      }
-      else
-      {
-       echo '<div class="alert alert-danger" role="alert"> Error</div>';
-      }
-      break;
-        case "Delete":
-      if (deleteStudentWithAwards($_POST['tid'])) 
-      {
-            echo '<div class="alert alert-success" role="alert"> Treatment successfully deleted!</div>';
-        } 
-      else {
-    echo '<div class="alert alert-danger" role="alert"> Error</div>';
-        }  
-  }
-}
+
 $houses = selectHouses();
 include "view-house.php";
 ?>
