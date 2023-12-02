f9ee9ecdaee14cef3016ac47bdc4fe95a3cde795
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 12:05:09 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -138,6 +138,10 @@ li {
             line-height: 1.5;
             color: black;
         }
+        .modal-header .btn-close {
+    padding: calc(var(--bs-modal-header-padding-y) * .5) calc(var(--bs-modal-header-padding-x) * .5);
+    margin: calc(-.5 * var(--bs-modal-header-padding-y)) calc(-.5 * var(--bs-modal-header-padding-x)) calc(-.5 * var(--bs-modal-header-padding-y)) auto;
+        }
         
     </style>
 </head>
