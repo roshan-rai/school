efc6b30307f857b5f59f39733614fc972a375218
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-03 00:45:03 -0600

    Update awards.php

--- awards.php
+++ awards.php
@@ -6,7 +6,7 @@ $pageTitle = "Awards";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if(insertAward($_POST['aName']$_POST['sHouse']))
+      if(insertAward($_POST['aName'],$_POST['sHouse']))
       {
         echo '<div class="alert alert-success" role="alert"> Award successfully added!</div>';
       }
