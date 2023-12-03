e6df96d32858a752906ad15d10d06252a7bb7608
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 18:13:59 -0600

    Update view-students-by-house.php

--- view-students-by-house.php
+++ view-students-by-house.php
@@ -1,45 +1,27 @@
-<div class="position-fixed bottom-0 end-0 p-3">
-        <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-                <strong class="me-auto">Student for House</strong>
-                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
-            <div class="toast-body">
-                 <div class="table-responsive">
-                  <table class="table">
-                    <thead>
-                      <tr>
-                         <th>Student ID</th>
-                      <th>Student Name</th>
-                      <th>Class</th>
-                      </tr>
-                     
-                    </thead>
-                      <tbody>
-                        <?php
-                            while ($student = $students-> fetch_assoc())
-                              {
-                                ?>
-                                  <tr>
-                                    <td><?php echo $student['student_id'];?></td>
-                                    <td><?php echo $student['student_name'];?></td>
-                                    <td> <?php echo $student['student_class'];?></td>
-                                  </tr>                  
-                                <?php
-                              }
-                        ?>
-                      </tbody>    
-                  </table>
-            </div>
-            </div>
-        </div>
-    </div>
 
-    <script>
-        // Show the toast when the "Students" button is clicked
-        document.getElementById('liveToastBtn').addEventListener('click', function () {
-            var toast = new bootstrap.Toast(document.getElementById('liveToast'));
-            toast.show();
-        });
-    </script>
-
+ <div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+         <th>Student ID</th>
+      <th>Student Name</th>
+      <th>Class</th>
+      </tr>
+     
+    </thead>
+      <tbody>
+        <?php
+            while ($student = $students-> fetch_assoc())
+              {
+                ?>
+                  <tr>
+                    <td><?php echo $student['student_id'];?></td>
+                    <td><?php echo $student['student_name'];?></td>
+                    <td> <?php echo $student['student_class'];?></td>
+                  </tr>                  
+                <?php
+              }
+        ?>
+      </tbody>    
+  </table>
+</div>
