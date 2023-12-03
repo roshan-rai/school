37ffe3bae533952190aabead7aa4ba496a364d4c
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 18:07:07 -0600

    Update house.php

--- house.php
+++ house.php
@@ -3,20 +3,6 @@ require_once("util-db.php");
 require_once("model-house.php");
 
 $pageTitle = "House";
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Display":
-      if(selectStudentsByHouse($_POST['hid']))
-      {
-        echo '<div class="alert alert-success" role="alert"> Student successfully displayed!</div>';
-      }
-      else
-      {
-       echo '<div class="alert alert-danger" role="alert"> Error</div>';
-      }
-      break;  
-}
-}
 $houses = selectHouses();
 include "view-house.php";
 ?>
