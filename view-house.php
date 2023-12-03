d11d302450e67ada21a8c9c77bcaabec227eacb5
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 18:53:10 -0600

    Update view-house.php

--- view-house.php
+++ view-house.php
@@ -18,32 +18,12 @@
                         <button type="submit" class="btn btn-primary" id="liveToastBtn">Students</button>
                     </form>
                 </div>
-                
-                <!-- Like Section -->
+
                <div>
                     <span class="like" data-action="upvote" data-house-id="<?php echo $house['house_id']; ?>" onclick="handleVote(this)">👍</span>
                     <span class="like" data-action="downvote" data-house-id="<?php echo $house['house_id']; ?>" onclick="handleVote(this)">👎</span>
-                    <span class="likes-count" id="likes-count-<?php echo $house['house_id']; ?>">0</span> likes
-                </div>
-
-                <!-- Comment Section -->
-                <div>
-                    <h5>Comments</h5>
-                    <!-- Add your comment form here -->
-                    <!-- Example: -->
-                    <form class="comment-form" data-house-id="<?php echo $house['house_id']; ?>">
-                        <textarea name="comment" placeholder="Type your comment here"></textarea>
-                        <button type="submit">Submit Comment</button>
-                    </form>
-
-                    <!-- Display existing comments here -->
-                    <!-- Example: -->
-                    <div class="comments-container" id="comments-container-<?php echo $house['house_id']; ?>">
-                        <!-- Comments will be displayed here -->
-                    </div>
-                </div>
-
-                
+                    <span class="likes-count" id="likes-count-<?php echo $house['house_id']; ?>">0</span> Votes
+                </div>           
             </div>
         </div>
 
@@ -58,29 +38,12 @@
         const action = element.getAttribute('data-action');
         const likesCountElement = document.getElementById(`likes-count-${houseId}`);
 
-        // Simulate updating likes count
         let currentLikes = parseInt(likesCountElement.innerText);
         if (action === 'upvote') {
             currentLikes++;
         } else if (action === 'downvote') {
             currentLikes--;
         }
-
-        // Update likes count on the page
         likesCountElement.innerText = currentLikes;
-    }
-
-    function handleComment(formElement) {
-        const houseId = formElement.getAttribute('data-house-id');
-        const commentTextarea = formElement.querySelector('textarea');
-        const commentsContainer = document.getElementById(`comments-container-${houseId}`);
-
-        // Simulate adding a new comment
-        const newComment = document.createElement('div');
-        newComment.textContent = commentTextarea.value;
-        commentsContainer.appendChild(newComment);
-
-        // Clear the comment textarea
-        commentTextarea.value = '';
     }
 </script>
