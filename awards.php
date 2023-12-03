76525541e2dbf41cb67d11ad60fe8af825679b17
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-03 00:55:08 -0600

    Update awards.php

--- awards.php
+++ awards.php
@@ -5,7 +5,7 @@ require_once("model-awards.php");
 $pageTitle = "Awards";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
-    case "Add":
+    case "AddAward":
       if(insertAward($_POST['aName'],$_POST['sHouse']))
       {
         echo '<div class="alert alert-success" role="alert"> Award successfully added!</div>';
