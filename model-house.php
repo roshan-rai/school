1ff225c9bd243a46213525b911147f6cdb15dd2d
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 18:06:50 -0600

    Update model-house.php

--- model-house.php
+++ model-house.php
@@ -45,21 +45,6 @@ function updateAwardWithHouse($tid, $did, $pid, $treatment_name, $sDate, $eDate)
     }
 }
 
-function selectStudentsByHouse($hid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT s.student_name,s.student_id,s.student_class,h.house_name FROM `student` s join house h on s.house_id=h.house_id where s.house_id=?");
-        $stmt->bind_param("i", $hid);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
 
 
 ?>
