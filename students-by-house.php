3e469e74ac09d5dbcfd685ccbff9c4c973fc933d
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 18:06:08 -0600

    Update students-by-house.php

--- students-by-house.php
+++ students-by-house.php
@@ -1,8 +1,10 @@
 <?php
 require_once("util-db.php");
 require_once("model-students-by-house.php");
+include "view-header.php";
 
 $pageTitle = "Student By House";
 $students = selectStudentsByHouse($_POST['hid']);
 include "view-students-by-house.php";
+include "view-footer.php";
 ?>
