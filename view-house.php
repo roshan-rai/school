19a616e61846b1b9ee25a26cff0148b14e59c462
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 18:01:56 -0600

    Update view-house.php

--- view-house.php
+++ view-house.php
@@ -32,7 +32,7 @@
     <div class="position-fixed bottom-0 end-0 p-3">
         <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
-                <strong class="me-auto">Notification</strong>
+                <strong class="me-auto">Students for <?php echo $house['house_name']; ?></strong>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
