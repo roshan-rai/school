a1a411127fdafdc0205a6a823279f562fb4690ca
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-02 18:04:24 -0600

    Update view-house.php

--- view-house.php
+++ view-house.php
@@ -13,11 +13,9 @@
                     <p class="card-text">Captain: <?php echo $house['house_captain']; ?></p>
                     <p class="card-text">Color: <?php echo $house['house_color']; ?></p>
                     <div>
-                        <form method="post" action="">
+                        <form method="post" action="students-by-house.php">
                             <input type="hidden" name="hid" value="<?php echo $house['house_id']; ?>" />
-                            <input type="hidden" name="actionType" value="Display">
-                            <button type="submit" class="btn btn-primary" id="liveToastBtn">StudentsOld</button>
-                            <button type="button" class="btn btn-primary" id="liveToastBtn">Students</button>
+                            <button type="submit" class="btn btn-primary" id="liveToastBtn">Students</button>
                         </form>
                     </div>
                 </div>
@@ -27,49 +25,3 @@
         }
         ?>
     </div>
-
-    <!-- Toast Container -->
-    <div class="position-fixed bottom-0 end-0 p-3">
-        <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-                <strong class="me-auto">Students for <?php echo $house['house_name']; ?></strong>
-                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
-            <div class="toast-body">
-                 <div class="table-responsive">
-                  <table class="table">
-                    <thead>
-                      <tr>
-                         <th>Student ID</th>
-                      <th>Student Name</th>
-                      <th>Class</th>
-                      </tr>
-                     
-                    </thead>
-                      <tbody>
-                        <?php
-                            while ($student = $students-> fetch_assoc())
-                              {
-                                ?>
-                                  <tr>
-                                    <td><?php echo $student['student_id'];?></td>
-                                    <td><?php echo $student['student_name'];?></td>
-                                    <td> <?php echo $student['student_class'];?></td>
-                                  </tr>                  
-                                <?php
-                              }
-                        ?>
-                      </tbody>    
-                  </table>
-            </div>
-            </div>
-        </div>
-    </div>
-
-    <script>
-        // Show the toast when the "Students" button is clicked
-        document.getElementById('liveToastBtn').addEventListener('click', function () {
-            var toast = new bootstrap.Toast(document.getElementById('liveToast'));
-            toast.show();
-        });
-    </script>
