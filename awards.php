8693a8e00a0d54be7fbb7dc4d3776e0f990bbfde
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-03 00:43:35 -0600

    Update awards.php

--- awards.php
+++ awards.php
@@ -3,6 +3,21 @@ require_once("util-db.php");
 require_once("model-awards.php");
 
 $pageTitle = "Awards";
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if(insertAward($_POST['aName']$_POST['sHouse']))
+      {
+        echo '<div class="alert alert-success" role="alert"> Award successfully added!</div>';
+      }
+      else
+      {
+       echo '<div class="alert alert-danger" role="alert"> Error</div>';
+      }
+      break;  
+}
+}
+
 
 $awards = selectAwards();
 include "view-awards.php";
