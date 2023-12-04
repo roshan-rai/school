681d12146826d0743a35372b470c115255d2930e
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-04 14:38:09 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,9 +1,42 @@
+<style>
+    h1 {
+      text-align: center;
+      padding: 50px 0;
+      color: #333; /* Dark gray text color */
+    }
+
+    .content {
+      max-width: 800px;
+      margin: 20px auto;
+      padding: 20px;
+      background-color: #fff; /* White background color for content */
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Box shadow for a subtle effect */
+    }
+
+    p {
+      font-size: 18px;
+      line-height: 1.6;
+      color: #333; /* Dark gray text color */
+    }
+
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+
+    li {
+      margin-bottom: 10px;
+    }
+  </style>
+
 <h1>About Us</h1>
+
   <div class="background-container"></div>
 
   <div class="content">
-    <p style="font-size: 50px;">
-      <ul>        <li>Hogwarts School of Witchcraft and Wizardry, nestled amidst the enchanting landscape of the British countryside, is a place where magic comes to life and young witches and wizards embark on an extraordinary journey of discovery.</li>
+    <p style="font-size: 24px;">
+      <ul>
+        <li>Hogwarts School of Witchcraft and Wizardry, nestled amidst the enchanting landscape of the British countryside, is a place where magic comes to life and young witches and wizards embark on an extraordinary journey of discovery.</li>
         <li>Location: British Old Castle</li>
         <li>Headmaster: Albus Dumbledore</li>
         <li>Means of Transportation: Hogwarts Express, Platform 9¾</li>
