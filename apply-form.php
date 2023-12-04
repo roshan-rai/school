0f15784c62a2bf6ff624fdbb71db8fad2b91a639
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-04 16:37:11 -0600

    Update apply-form.php

--- apply-form.php
+++ apply-form.php
@@ -1,4 +1,10 @@
-<button type="button" class="btn btn-primary btn-lg" onclick="window.location.href='http://school.roshan.oucreate.com/'">Back to Main Page</button>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newStudentModal">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+    </svg>
+</button>
 
 <!-- Modal -->
 <div class="modal fade" id="newStudentModal" tabindex="-1" aria-labelledby="newStudentModalLabel" aria-hidden="true">
@@ -9,32 +15,14 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form method="post" action="">
+                <form method="post" action="" onsubmit="submitForm(event)">
+                    <!-- Your form fields... -->
                     <div class="mb-3">
                         <label for="sName" class="form-label">Student Name</label>
                         <input type="text" class="form-control" id="sName" name="sName" required>
                     </div>
-                    <div class="mb-3">
-                        <label for="sAddress" class="form-label">Address</label>
-                        <textarea class="form-control" id="sAddress" name="sAddress" rows="3" required></textarea>
-                    </div>
-                  
-                    <div class="mb-3">
-                        <label for="sDOB" class="form-label">Date of Birth</label>
-                        <input type="date" class="form-control" id="sDOB" name="sDOB" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="sClass" class="form-label">Student Class</label>
-                        <input type="text" class="form-control" id="sClass" name="sClass" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="sEssay" class="form-label">Write in 600 words why you deserve a place in Hogwarts.</label>
-                        <textarea class="form-control" id="sEssay" name="sEssay" rows="6" required></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <label for="sPreferredHouse" class="form-label">House you prefer to be in</label>
-                        <input type="text" class="form-control" id="sPreferredHouse" name="sPreferredHouse" required>
-                    </div>
+                    <!-- ... other form fields ... -->
+
                     <input type="hidden" name="actionType" value="Add">
                     <button type="submit" class="btn btn-success">Submit Form</button>
                 </form>
@@ -42,3 +30,19 @@
         </div>
     </div>
 </div>
+
+<script>
+    function submitForm(event) {
+        // Prevent the default form submission
+        event.preventDefault();
+
+        // Display congratulations message
+        alert("Congratulations! Your form has been submitted. You will receive an Owl soon from Hogwarts Admissions.");
+
+        document.body.style.backgroundImage = "url('paperbackground.jpg')";
+        document.body.style.backgroundSize = "cover";
+
+        // Redirect to the specified URL
+        window.location.href = "http://school.roshan.oucreate.com/";
+    }
+</script>
