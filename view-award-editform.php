b894fe1d5a5021d766748f99ad7436f145c2c453
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-12-04 14:17:32 -0600

    Update view-award-editform.php

--- view-award-editform.php
+++ view-award-editform.php
@@ -5,28 +5,29 @@
     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
 </svg>
+</button>
 
 <!-- Modal -->
-<div class="modal fade" id="editAwardModal" tabindex="-1" aria-labelledby="editAwardModalLabel" aria-hidden="true">
+<div class="modal fade" id="editAwardModal<?php echo $award['award_id']; ?>" tabindex="-1" aria-labelledby="editAwardModalLabel<?php echo $award['award_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editAwardModalLabel">Edit Awards</h1>
+        <h1 class="modal-title fs-5" id="editAwardModalLabel<?php echo $award['award_id']; ?>">Edit Awards</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
           <form method="post" action="">
             <div class="mb-3">
-              <label for="aName" class="form-label">Award Name</label>
-              <input type="text" class="form-control" id="aName" name="aName">
-            </div>
-                <div class="mb-3">
-              <label for="sHouse" class="form-label">House</label>
-                  <?php $houseList=selectHousesForInput(); 
-                        $selectedHouse = 0;
+              <label for="aName<?php echo $award['award_id']; ?>" class="form-label">Award Name</label>
+              <input type="text" class="form-control" id="aName<?php echo $award['award_id']; ?>" name="aName" value="<?php echo $award['award_name']; ?>">
+            </div>          
+              <div class="mb-3">
+              <label for="sHouse<?php echo $award['award_id']; ?>" class="form-label">House</label>
+                 <?php $houseList=selectHousesForInput(); 
+                        $selectedHouse = $award['house_id'];
                   include "view-house-input-list.php"; ?>
             </div>
-                  <input type="hidden" name="aid" value="<?php echo $award['award_id']; ?>">
+                <input type="hidden" name="aid" value="<?php echo $award['award_id']; ?>">
                 <input type="hidden" name="actionType" value="EditAward">
             <button type="submit" class="btn btn-primary">Save</button>
           </form>
